f89622f0729751abfa491d2b67807220d0b17e3b
andre <andremaivelasco@hotmail.com>
2023-08-14 20:05:14 -0300

    service cliente pronta

--- src/main/java/com/pizzaria/PizzariaAPI/Service/ClienteService.java
+++ src/main/java/com/pizzaria/PizzariaAPI/Service/ClienteService.java
@@ -6,6 +6,7 @@ import com.pizzaria.PizzariaAPI.Repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +33,44 @@ public class ClienteService {
 
     @Transactional(rollbackFor = Exception.class)
     public void create(final ClienteDTO clienteDTO){
+        Assert.isTrue(!clienteDTO.getNome().isBlank(), "Nome inválido!");
 
+        Assert.notNull(clienteDTO.getIdade(), "Idade não pode ser nula!");
+        Assert.isTrue(clienteDTO.getIdade() > 0, "Idade não pode ser negativa!");
+
+        Assert.isTrue(!clienteDTO.getEmail().isBlank(), "Deve conter email!");
+        Assert.isTrue(clienteDTO.getEmail().matches("[a-zA-Z0-9]+@[a-z]+[.]{1}[a-z]+"), "Formato do email inválido!");
+
+        Assert.isTrue(!clienteDTO.getSenha().isBlank(), "Deve conter senha!");
+
+        Cliente cliente = toCliente(clienteDTO);
+        this.clienteRepository.save(cliente);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void update(final Long id, final ClienteDTO clienteDTO){
+        ClienteDTO clienteDatabase = findById(id);
+        Assert.notNull(clienteDatabase, "Cliente não encontrado!");
+        Assert.isTrue(clienteDatabase.getId().equals(clienteDTO.getId()), "Clientes não conferem!");
+
+        Assert.isTrue(!clienteDTO.getNome().isBlank(), "Nome inválido!");
+
+        Assert.notNull(clienteDTO.getIdade(), "Idade não pode ser nula!");
+        Assert.isTrue(clienteDTO.getIdade() > 0, "Idade não pode ser negativa!");
+
+        Assert.isTrue(!clienteDTO.getEmail().isBlank(), "Deve conter email!");
+        Assert.isTrue(clienteDTO.getEmail().matches("[a-zA-Z0-9]+@[a-z]+[.]{1}[a-z]+"), "Formato do email inválido!");
+
+        Assert.isTrue(!clienteDTO.getSenha().isBlank(), "Deve conter senha!");
+
+        Cliente cliente = toCliente(clienteDTO);
+        this.clienteRepository.save(cliente);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void delete(final Long id){
+        Cliente cliente = toCliente(findById(id));
+        this.clienteRepository.delete(cliente);
     }
 
     public ClienteDTO toClienteDTO(final Cliente cliente){
