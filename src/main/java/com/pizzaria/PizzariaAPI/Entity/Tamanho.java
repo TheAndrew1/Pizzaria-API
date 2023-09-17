7b4e6ae4ba77a85cfd7124a2186d06d351be14a9
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-09-17 15:30:06 -0300

    enum Tamanho ajustado

--- src/main/java/com/pizzaria/PizzariaAPI/Entity/Tamanho.java
+++ src/main/java/com/pizzaria/PizzariaAPI/Entity/Tamanho.java
@@ -1,5 +1,16 @@
 package com.pizzaria.PizzariaAPI.Entity;
 
+import lombok.Getter;
+
+@Getter
 public enum Tamanho {
-    P, M, G, GG
+    P("P", 1), M("M", 2), G("G", 3), GG("GG", 4);
+
+    String tamanho;
+    int quantidadeSabores;
+
+    Tamanho(String tamanho, int quantidadeSabores) {
+        this.tamanho = tamanho;
+        this.quantidadeSabores = quantidadeSabores;
+    }
 }
