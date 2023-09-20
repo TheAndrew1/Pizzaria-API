50995177b53683cac7ec87725087cfbed8514b51
andre <andremaivelasco@hotmail.com>
2023-09-20 19:40:48 -0300

    teste controllerSabor

--- src/test/java/com/pizzaria/pizzaria_api/testes/controller/SaborControllerTest.java
+++ src/test/java/com/pizzaria/pizzaria_api/testes/controller/SaborControllerTest.java
@@ -1,15 +1,17 @@
 package com.pizzaria.pizzaria_api.testes.controller;
 
 import com.pizzaria.pizzaria_api.controller.SaborController;
+import com.pizzaria.pizzaria_api.dto.SaborDTO;
 import com.pizzaria.pizzaria_api.entity.Sabor;
 import com.pizzaria.pizzaria_api.repository.SaborRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +20,7 @@ import java.util.Optional;
 public class SaborControllerTest {
     @Autowired
     SaborController saborController;
-    @Mock
+    @MockBean
     SaborRepository saborRepository;
 
     @BeforeEach
@@ -30,14 +32,58 @@ public class SaborControllerTest {
 
         Mockito.when(saborRepository.findById(1L)).thenReturn(Optional.of(sabor1));
         Mockito.when(saborRepository.findAll()).thenReturn(sabores);
+        Mockito.when(saborRepository.save(sabor1)).thenReturn(sabor1);
+        Mockito.when(saborRepository.save(sabor1)).thenReturn(sabor1);
     }
 
     @Test
     void TestControllerFindById(){
-        var sabor = saborController.findById(1L);
-        Long id = sabor.getBody().getId();
-        String nomeSabor = sabor.getBody().getNome();
+        var resposta = saborController.findById(1L);
+        Long id = resposta.getBody().getId();
+        String nomeSabor = resposta.getBody().getNome();
+        Assertions.assertEquals(HttpStatus.OK, resposta.getStatusCode());
         Assertions.assertEquals(1L, id);
         Assertions.assertEquals("Brócolis", nomeSabor);
+    }
+
+    @Test
+    void TestControllerFindByAll(){
+        var resposta = saborController.findAll();
+        int quantidade = resposta.getBody().size();
+
+        Assertions.assertEquals(HttpStatus.OK, resposta.getStatusCode());
+        Assertions.assertEquals(3, quantidade);
+    }
+
+    @Test
+    void TestControllerCreate(){
+        SaborDTO saborCriado = new SaborDTO(1L, "Brócolis", "Queijo, brócoclis e catupiri", 18.00, null);
+        String mensagem = "Cadastrado com sucesso!";
+
+        var resposta = saborController.create(saborCriado);
+
+        Assertions.assertEquals(HttpStatus.OK, resposta.getStatusCode());
+        Assertions.assertEquals(mensagem, resposta.getBody());
+    }
+
+    @Test
+    void TestControllerUpdate(){
+        SaborDTO saborEditado = new SaborDTO(1L, "Brócolis", "Queijo, brócoclis e catupiri", 20.00, null);
+        String mensagem = "Editado com sucesso!";
+
+        var resposta = saborController.update(1L, saborEditado);
+
+        Assertions.assertEquals(HttpStatus.OK, resposta.getStatusCode());
+        Assertions.assertEquals(mensagem, resposta.getBody());
+    }
+
+    @Test
+    void TestControllerDelete(){
+        String mensagem = "Exlcuido com sucesso!";
+
+        var resposta = saborController.delete(1L);
+
+        Assertions.assertEquals(HttpStatus.OK, resposta.getStatusCode());
+        Assertions.assertEquals(mensagem, resposta.getBody());
     }
 }
